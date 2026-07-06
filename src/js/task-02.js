2dff8de6c77037dd94b9ebc01eebe1f0f798da8b
AndreySavin <AndreySavin4@ukr.net>
2026-07-06 08:36:36 +0200

    complete and optimize task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+const ingredientsList = (ingredients) => {
+  return ingredients.map((el) => {
+    const elementLi = document.createElement("li");
+    elementLi.classList.add("item");
+    elementLi.textContent = el;
+
+    return elementLi;
+  });
+};
+
+list.append(...ingredientsList(ingredients));
