9e1ff1fb735c681ce70c810185f570a1be224840
AndreySavin <AndreySavin4@ukr.net>
2026-07-09 20:29:02 +0200

    complete task 8, submit the form

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", (e) => {
+  const {
+    elements: { email, password },
+  } = form;
+
+  e.preventDefault();
+
+  if (email.value.trim() === "" || password.value.trim() === "") {
+    alert("Please fill in all the fields of the form!");
+    return;
+  }
+
+  console.log({
+    email: email.value,
+    password: password.value,
+  });
+  form.reset();
+});
