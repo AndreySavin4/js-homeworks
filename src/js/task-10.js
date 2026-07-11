cef79f0fdd11e05a44e8c155cc6979c6c815b16e
AndreySavin <AndreySavin4@ukr.net>
2026-07-11 09:23:20 +0200

    implement task 10 markup and logic

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,5 +1,40 @@
-function getRandomHexColor() {
+const getRandomHexColor = () => {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
+
+const input = document.querySelector("#controls input");
+const btnCreate = document.querySelector("button[data-create]");
+const btnDestroy = document.querySelector("button[data-destroy]");
+const boxes = document.querySelector("#boxes");
+
+let width = 30;
+let height = 30;
+
+const createBoxes = (amount) => {
+  for (let i = 0; i < amount; i += 1) {
+    const div = document.createElement("div");
+    div.style.width = `${width}px`;
+    div.style.height = `${height}px`;
+    div.style.backgroundColor = getRandomHexColor();
+
+    boxes.append(div);
+
+    width += 10;
+    height += 10;
+  }
+};
+
+const destroyBoxes = () => (boxes.innerHTML = "");
+
+btnCreate.addEventListener("click", () => {
+  createBoxes(input.value);
+});
+
+btnDestroy.addEventListener("click", () => {
+  destroyBoxes();
+
+  width = 30;
+  height = 30;
+});
